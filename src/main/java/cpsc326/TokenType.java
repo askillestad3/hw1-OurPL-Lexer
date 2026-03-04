3db34e6688775dcc4d6d7a4668d074a2a2f743cf
Abby Skillestad <askillestad@zagmail.gonzaga.edu>
2026-03-04 11:28:33 -0800

    update token types

--- src/main/java/cpsc326/TokenType.java
+++ src/main/java/cpsc326/TokenType.java
@@ -1,6 +1,18 @@
 package cpsc326;
 
 enum TokenType {
-    EOF, AND
-        // TODO: add the token types needed by the OurPL lexer.
+    // groups
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    // operations and punctuation
+    COMMA, DOT, PLUS, MINUS, STAR, SLASH, SEMICOLON,
+    // assignment and comparators
+    EQUAL, EQUAL_EQUAL, BANG, BANG_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+    // reserved keywords
+    AND, ELSE, FALSE, FOR, FUN, IF, NIL, OR, PRINT,  RETURN,
+    STRUCT, THIS, TRUE, VAR, WHILE,
+    // end of file
+    EOF
 }
