816d83c3bd0e7378736e2a3281e9cab92806818e
Abby Skillestad <askillestad@zagmail.gonzaga.edu>
2026-04-16 12:09:02 -0700

    add visitor interface to Stmt class

--- src/main/java/cpsc326/Stmt.java
+++ src/main/java/cpsc326/Stmt.java
@@ -3,13 +3,18 @@ package cpsc326;
 abstract class Stmt {
     interface Visitor<R> {
         R visitPrintStmt(Print stmt);
-        R visitExpressionStmt(Expr stmt);
+        R visitExpressionStmt(Expression stmt);
     }
     static class Print extends Stmt {
         Expr expression;
 
         Print(Expr expr) {
             expression = expr;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitPrintStmt(this);
         }
     }
     static class Expression extends Stmt {
@@ -18,5 +23,11 @@ abstract class Stmt {
         Expression(Expr expr) {
             expression = expr;
         }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
+        }
     }
+    abstract <R> R accept(Visitor<R> visitor);
 }
