20a4cc3da5a2d3287bd19f3633197642b1453010
Abby Skillestad <askillestad@zagmail.gonzaga.edu>
2026-04-19 21:46:24 -0700

    add Stmt.java subclasses and update visitor interface

--- src/main/java/cpsc326/Stmt.java
+++ src/main/java/cpsc326/Stmt.java
@@ -3,7 +3,16 @@ package cpsc326;
 abstract class Stmt {
     interface Visitor<R> {
         R visitPrintStmt(Print stmt);
+
         R visitExpressionStmt(Expression stmt);
+
+        R visitBlockStmt(Block stmt);
+
+        R visitIfStmt(If stmt);
+
+        R visitVarStmt(Var stmt);
+
+        R visitWhileStmt(While stmt);
     }
     static class Print extends Stmt {
         Expr expression;
@@ -29,5 +38,22 @@ abstract class Stmt {
             return visitor.visitExpressionStmt(this);
         }
     }
+
+    static class Block extends Stmt {
+
+    }
+
+    static class If extends Stmt {
+
+    }
+
+    static class Var extends Stmt {
+
+    }
+
+    static class While extends Stmt {
+        
+    }
+
     abstract <R> R accept(Visitor<R> visitor);
 }
