db375ba3036fb167dda0c0b063bdb5dab9aa4672
Abby Skillestad <askillestad@zagmail.gonzaga.edu>
2026-04-27 11:01:33 -0700

    start of homework 4 - add new files

---
+++ src/main/java/cpsc326/OurPLFunction.java
@@ -0,0 +1,26 @@
+package cpsc326;
+
+import java.util.List;
+
+class OurPLFunction implements OurPLCallable{
+    private final Stmt.Function declaration;
+
+    OurPLFunction(Stmt.Function declaration) {
+        this.declaration = declaration;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        
+    }
+
+    @Override
+    public int arity() {
+        
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
